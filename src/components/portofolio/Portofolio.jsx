bf23f6996419040b1785a8b8194f88a6105ce609
kariman <kariman0800@gmail.com>
2021-05-15 06:52:53 +0200

    portofolio section

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,28 +1,74 @@
-import './portofolio.scss'
+import "./portofolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useState, useEffect } from "react";
+import {
+  fullStackPortfolio,
+  backEndPortfolio,
+  frontEndPortfolio,
+  All
+} from "../../data";
 
 export default function Portofolio() {
-    return (
-        <div className="portofolio" id="portfolio">
-            <h1>Portfolio</h1>
-            <ul>
-                <li className="active">Full Stack</li>
-                <li>Front End</li>
-                <li>Back End</li>
-            </ul>
-            <div className="container">
-                <div className="item">
-                    <img src="assets/il.png" alt=""/>
-                    <h3>project</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/il.png" alt=""/>
-                    <h3>project</h3>
-                </div>
-                <div className="item">
-                    <img src="assets/il.png" alt=""/>
-                    <h3>project</h3>
-                </div>
-            </div>
-        </div>
-    )
+  const [selected, setSelected] = useState("all");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    switch (selected) {
+      case "all" :
+          setData(All)
+          break;
+          
+      case "fullStack":
+        setData(fullStackPortfolio);
+        break;
+
+      case "frontend":
+        setData(frontEndPortfolio);
+        break;
+
+      case "backend":
+        setData(backEndPortfolio);
+    }
+  }, [selected]);
+  const items = [
+    {
+      id: "all",
+      title: "All",
+    },
+    {
+      id: "fullStack",
+      title: "Full Satck",
+    },
+    {
+      id: "frontend",
+      title: "Front End",
+    },
+    {
+      id: "backend",
+      title: "Back End",
+    },
+  ];
+  return (
+    <div className="portofolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {items.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
