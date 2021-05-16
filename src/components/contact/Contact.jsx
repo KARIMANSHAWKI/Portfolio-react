d9489353506aeccea18a6cbec53d0dec1d0b301f
kariman <kariman0800@gmail.com>
2021-05-17 00:08:40 +0200

    add works section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,15 @@ import './contact.scss'
 export default function Contact() {
     return (
         <div className="contact" id="contact">
+            <div className="left">
+                <img src="assets/shaek.svg" alt="shake"/>
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
         </div>
     )
 }
